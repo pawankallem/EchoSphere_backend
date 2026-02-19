8dc195756da19e95fc8f05438459d1f0fc8a4323
pavankallempinaca <pavan.k@pinacagroup.com>
2026-02-20 01:51:36 +0530

    added vercel config

--- api/index.py
+++ api/index.py
@@ -3,13 +3,9 @@ from routes.auth_routes import auth_bp
 
 app = Flask(__name__)
 
+# register routes
 app.register_blueprint(auth_bp, url_prefix="/api/auth")
 
 @app.route("/")
 def home():
-    return {"status": "API running"}
-
-# Vercel handler
-def handler(request, context):
-    return app(request.environ, start_response=context.start_response)
-
+    return {"status": "EchoSphere backend running"}
